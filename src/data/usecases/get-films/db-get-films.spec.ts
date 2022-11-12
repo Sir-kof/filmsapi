c80fffe371a587e1bf53cc5cfb51467b707230fc
Rodrigo Fernandes <rodrigo_mdg_fernandes@outlook.com>
2022-11-12 15:54:02 -0300

    mend

--- src/data/usecases/get-films/db-get-films.spec.ts
+++ src/data/usecases/get-films/db-get-films.spec.ts
@@ -1,55 +1,52 @@
-// import { FilmModel } from '../../../domain/models/Film'
-// import { GetFilmModel } from '../../../domain/models/GetFilms'
-// import { IGetFilmsRepository } from '../../protocols/db/get-films-repository'
-// import { DbGetFilms } from './db-get-films'
+import { FilmModel } from '../../../domain/models/Film'
+import { GetFilmModel } from '../../../domain/models/GetFilms'
+import { IGetFilmsRepository } from '../../protocols/db/get-films-repository'
+import { DbGetFilms } from './db-get-films'
 
-// const makeGetFilm = (): IGetFilmsRepository => {
-//   class AddFilmStub implements IGetFilmsRepository {
-//     async getFilms (): Promise<GetFilmModel> {
-//       return await new Promise(resolve => resolve({
-//         title: 'valid_title',
-//         original_title: 'valid_original_title',
-//         description: 'valid_description',
-//         release_date: 'valid_date',
-//         rt_score: 'valid_score'
-//       }))
-//     }
-//   }
-//   return new AddFilmStub()
-// }
+import env from "../../../main/config/env"
 
-// const makeFakeFilm = (): FilmModel => ({
-//   title: 'valid_title',
-//   original_title: 'valid_original_title',
-//   description: 'valid_description',
-//   release_date: 'valid_date',
-//   rt_score: 'valid_score'
-// })
+const makeGetFilm = (): IGetFilmsRepository => {
+  class AddFilmStub implements IGetFilmsRepository {
+    async getFilms (): Promise<GetFilmModel> {
+      return await new Promise(resolve => resolve({
+        title: 'valid_title',
+        original_title: 'valid_original_title',
+        description: 'valid_description',
+        release_date: 'valid_date',
+        rt_score: 'valid_score'
+      }))
+    }
+  }
+  return new AddFilmStub()
+}
 
-// interface SutTypes {
-//   sut: DbGetFilms
-//   getFilmsStub: IGetFilmsRepository
-// }
+const makeFakeFilm = (): FilmModel => ({
+  title: 'valid_title',
+  original_title: 'valid_original_title',
+  description: 'valid_description',
+  release_date: 'valid_date',
+  rt_score: 'valid_score'
+})
 
-// const makeSut = (): SutTypes => {
-//   const getFilmsStub = makeGetFilm()
-//   const sut = new DbGetFilms(getFilmsStub)
-//   return {
-//     sut,
-//     getFilmsStub
-//   }
-// }
+interface SutTypes {
+  sut: DbGetFilms
+  getFilmsStub: IGetFilmsRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getFilmsStub = makeGetFilm()
+  const sut = new DbGetFilms(getFilmsStub)
+  return {
+    sut,
+    getFilmsStub
+  }
+}
 
 describe('DbAddFilm Usecase', () => {
   test('Should return films with correct values specified', async () => {
     // aqui vai a url da nossa api
     // configurar env no main layer que está por vir para poder prosseguir com esse teste
-    // const res = await axios.get('https://ghibliapi.herokuapp.com/films?limit=200')
-    // expect(res.data.map((item: { hasOwnProperty: (arg0: string) => any }) => item.hasOwnProperty('title'))).toBeTruthy()
-    // expect(res.data.map((item: { hasOwnProperty: (arg0: string) => any }) => item.hasOwnProperty('original_title'))).toBeTruthy()
-    // expect(res.data.map((item: { hasOwnProperty: (arg0: string) => any }) => item.hasOwnProperty('description'))).toBeTruthy()
-    // expect(res.data.map((item: { hasOwnProperty: (arg0: string) => any }) => item.hasOwnProperty('release_date'))).toBeTruthy()
-    // expect(res.data.map((item: { hasOwnProperty: (arg0: string) => any }) => item.hasOwnProperty('rt_score'))).toBeTruthy()
+    // const res = await axios.get(env.ourUrlApi)
     expect(1).toBe(1)
   })
 })
--- src/infra/db/mysql/filmes/film-mysql-repository.ts
+++ src/infra/db/mysql/filmes/film-mysql-repository.ts
@@ -1,11 +1,12 @@
 import { IAddFilmRepository } from '../../../../data/protocols/db/add-films-repository'
 import { IGetFilmsRepository } from '../../../../data/protocols/db/get-films-repository'
 import { FilmModel } from '../../../../domain/models/Film'
+import env from '../../../../main/config/env'
 import { MysqlHelper, addFilmsDb } from '../helpers/mysql-helper'
 
 export class FilmMysqlRepository implements IAddFilmRepository, IGetFilmsRepository {
   async add (film: object): Promise<void> {
-    await addFilmsDb('env.url')
+    await addFilmsDb(env.urlSearchFilms)
   }
 
   async getFilms (): Promise<FilmModel> {
