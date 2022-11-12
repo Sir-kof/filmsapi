a8554364c6e99642dc1053aa942dd979955d4b4e
Rodrigo Fernandes <rodrigo_mdg_fernandes@outlook.com>
2022-11-12 16:07:39 -0300

    feat: config presentation layer

---
+++ src/presentation/controllers/get-films/get-films-controller.ts
@@ -0,0 +1,7 @@
+import { serverError } from '../../helpers/http/http-helper'
+import { MysqlHelper } from '../../../infra/db/mysql/helpers/mysql-helper'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const GetFilmsController = async (req, res) => {
+  return null
+}
---
+++ src/presentation/helpers/http/http-helper.ts
@@ -0,0 +1,18 @@
+// import { MissingParamError } from "../errors/missing-param-error"
+import { HttpResponse } from '../../protocols'
+import { ServerError } from '../../errors'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
